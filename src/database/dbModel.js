2c0562428fe9f8641812da843cc7f85778aa866b
Megan Loertscher <pearl@loertscher.org>
2025-02-04 12:29:36 -0700

    fix: only allow unique emails for users

--- src/database/dbModel.js
+++ src/database/dbModel.js
@@ -8,7 +8,8 @@ const tableCreateStatements = [
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    UNIQUE(email)
   )`,
 
   `CREATE TABLE IF NOT EXISTS menu (
