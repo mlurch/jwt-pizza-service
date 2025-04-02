400460c41c12b608c60c8f31e098d6b4ffac7d08
Megan Loertscher <pearl@loertscher.org>
2025-04-01 23:02:42 -0600

    feat: load test for loginAndOrder

---
+++ loadTests/loginAndOrder.mjs
@@ -0,0 +1,162 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js";
+
+export const options = {
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  const vars = {};
+
+  group("page_2 - https://pizza.meganhuff.click/", function () {
+    response = http.put(
+      "https://pizza-service.meganhuff.click/api/auth",
+      '{"email":"a@jwt.com","password":"admin"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          dnt: "1",
+          origin: "https://pizza.meganhuff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Not:A-Brand";v="24", "Chromium";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+
+    vars["token"] = jsonpath.query(response.json(), "$.token")[0];
+
+    sleep(4.1);
+
+    response = http.get(
+      "https://pizza-service.meganhuff.click/api/order/menu",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token"]}`,
+          "content-type": "application/json",
+          dnt: "1",
+          origin: "https://pizza.meganhuff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Not:A-Brand";v="24", "Chromium";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+
+    response = http.get("https://pizza-service.meganhuff.click/api/franchise", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${vars["token"]}`,
+        "content-type": "application/json",
+        dnt: "1",
+        origin: "https://pizza.meganhuff.click",
+        priority: "u=1, i",
+        "sec-ch-ua": '"Not:A-Brand";v="24", "Chromium";v="134"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+    sleep(5.9);
+
+    response = http.post(
+      "https://pizza-service.meganhuff.click/api/order",
+      '{"items":[{"menuId":1,"description":"Veggie","price":0.0038}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token"]}`,
+          "content-type": "application/json",
+          dnt: "1",
+          origin: "https://pizza.meganhuff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Not:A-Brand";v="24", "Chromium";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+    sleep(1.7);
+
+    vars["jwt"] = response.json().body.jwt;
+
+    response = http.post(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      `{"jwt":${vars.jwt}}`,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token"]}`,
+          "content-type": "application/json",
+          dnt: "1",
+          origin: "https://pizza.meganhuff.click",
+          priority: "u=1, i",
+          "sec-ch-ua": '"Not:A-Brand";v="24", "Chromium";v="134"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"macOS"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "sec-fetch-storage-access": "active",
+        },
+      },
+    );
+  });
+}
